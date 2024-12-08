d7048a7e90cfaf7bf6871810366660e531e6221c
Oscar Quintero <oquinterol@unal.edu.co>
2024-12-08 09:08:29 -0500

    arrow interactive

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     var arrow = document.querySelector('.arrow');
-    // Establece el estado inicial basado en la posición actual de desplazamiento
+    var container = document.querySelector('.container');
+
+    // Establece el estado inicial de la flecha
     arrow.style.display = window.scrollY > 100 ? 'none' : 'flex';
+
+    // Evento de clic en la flecha
+    arrow.addEventListener('click', function() {
+        // Scroll hacia el contenedor
+        if (container) {
+            container.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+    });
 });
 
 document.addEventListener('scroll', function() {
